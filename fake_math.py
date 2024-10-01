277c70133bc0e750034aa90b5b3643cff41b741c
Енот <68638093+Enots21@users.noreply.github.com>
2024-10-01 12:16:35 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+# ------------------------Функция
+def divide(first, second):
+    if second == 0:
+        return 'Ошибка'
+    return first / second
---
+++ true_math.py
@@ -0,0 +1,8 @@
+# ------------------------Модули
+import math as mt
+
+# ------------------------Функция
+def divide(first, second):
+    if second == 0:
+        return mt.inf
+    return first / second
